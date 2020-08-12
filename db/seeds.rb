36bf7a0cbaddd74561922443f957d10fd7e37778
stephanie rousset <strabon@club-internet.fr>
2020-08-12 16:13:27 +0200

    completed second exo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "creating 5 fake restaurants"
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: category.sample,
+    phone_number: Faker::PhoneNumber.phone_number
+    )
+  restaurant.save!
+end
+
+puts "done!"
